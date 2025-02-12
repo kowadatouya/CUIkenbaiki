e850d3edca8d2fff757022cdb512722cc0e39dcf
naho <n.takahashi.sys24@morijyobi.ac.jp>
2025-02-12 11:58:27 +0900

    CUI券売機シミュレータを作る_close #8

---
+++ source/admin.py
@@ -0,0 +1,84 @@
+import keyboard
+class Admin:
+    def __init__(self):
+        self.special = 1000
+        self.special_pieces = 0
+        self.special_sum = 0
+        self.soi = 780
+        self.soi_pieces = 0
+        self.soi_sum = 0
+        self.salt = 880
+        self.salt_pieces = 0
+        self.salt_sum = 0
+        self.rice = 150
+        self.rice_pieces = 0
+        self.rice_sum = 0
+    def get_key_input(self):
+        while True:
+            if keyboard.is_pressed('enter'):
+                return 'enter'
+            if keyboard.is_pressed('esc'):
+                return 'esc'
+    def menu(self):
+        print('***********************')
+        print('       管理画面')
+        print('***********************')
+        print('======= 商品一覧 =======')
+        print('商品      単価  販売数  売上金額')
+        print('=======================')
+        print(f'1.特製ラーメン {self.special}円  {self.special_pieces}   {self.special_sum}')
+        print(f'2.醤油ラーメン {self.soi}円   {self.soi_pieces}      {self.soi_sum}')
+        print(f'3.しおラーメン {self.salt}円     {self.salt_pieces}     {self.salt_sum}')
+        print(f'4.ごはん {self.rice}円      {self.rice_pieces}        {self.rice_sum} ')
+        print('———')
+        print('総売上金額 80,700円')
+        print('=== 管理メニュー ====')
+        print('1. 売上をリセットする')
+        print('2. 商品の価格を変更する')
+        print('※売上がリセットされていないと利用できません。')
+        print('3. 管理画面を終了する')
+        print('———')
+        self.admin_user()
+    def command1(self):
+        print("管理コード入力:1")
+        print("===")
+        self.special_pieces = 0
+        self.special_sum = 0
+        self.soi_pieces = 0
+        self.soi_sum = 0
+        self.salt_pieces = 0
+        self.salt_sum = 0
+        self.rice_pieces = 0
+        self.rice_sum = 0
+        print("売上をリセットしました。")
+        self.menu()
+    def command2(self):
+        print('管理コード入力:2')
+        print('======= 商品一覧 =======')
+        print('商品      単価  販売数  売上金額')
+        print('=======================')
+        print('1.特製ラーメン 1000円  0   0円')
+        print('2.醤油ラーメン 780円   0     0円')
+        print('3.しおラーメン 880円     0     0円')
+        print('4.ごはん 150円      0     0円')
+        print('———')
+        input('価格を変更する商品の番号を入力してください。>')
+        input('変更金額を入力してください。>')
+        print('【2.醤油ラーメン 800円】に変更します。')
+        print('よろしいですか(Y/N）>')
+        print('変更しました。')
+        self.menu()
+    def command3(self):
+        print('管理コード入力:3 （Enterキー押下でタイトル画面の表示に戻る）')
+        key = self.get_key_input()
+        if key == 'enter':
+            from human import Human
+            Human.title()
+    def admin_user(self):
+        admin = input()
+        if admin == 1:
+            self.command1()
+        elif admin == 2:
+            self.command2
+        elif admin == 3:
+            self.command3()
---
+++ source/human.py
@@ -0,0 +1,99 @@
+import keyboard
+from admin import Admin
+
+class Human:
+    def __init__(self):
+        self.items = {
+            "1": {"name": "特製ラーメン", "price": 1000},
+            "2": {"name": "醤油ラーメン", "price": 780},
+            "3": {"name": "しおラーメン", "price": 880},
+            "4": {"name": "ごはん", "price": 150}
+        }
+        self.items_in_cart = {}
+
+    def title(self):
+        print("***********************")
+        print("券売機シミュレータ")
+        print("***********************")
+        print("Please Enter (Enterキー押下で画面がクリアされて処理が進む）")
+        print("（ESCキー押下で管理画面に処理が進む）")
+        print("（qキー押下でシミュレータ終了）\n")
+
+    def shop(self):
+        print("商品      金額")
+        print("=======================")
+        for key, item in self.items.items():
+            print(f"{key}.{item["name"]} {item["price"]}円")
+        print("———")
+        while True:
+            human_select = input("購入する商品番号(支払いに進む場合はc)>")
+            if human_select in self.items:
+                if human_select in self.items_in_cart:
+                    self.items_in_cart[human_select] += 1
+                else:
+                    self.items_in_cart[human_select] = 1
+            elif human_select == "c":
+                self.show_cart()
+                self.pay()
+                break
+
+    def show_cart(self):
+        print("———")
+        print("商品        数量")
+        for item_id, quantity in self.items_in_cart.items():
+            item_name = self.items[item_id]["name"]
+            print(f"{item_id}.{item_name}   {quantity}")
+        total = self.total()
+        print("===")
+        print(f"合計{total}円")
+
+    def total(self):
+        total = 0
+        for item_id, quantity in self.items_in_cart.items():
+            total += self.items[item_id]["price"] * quantity
+        return total
+
+    def pay(self):
+        total = self.total()
+        while True:
+            try:
+                print("———")
+                payment = int(input("現金を投入してください>"))
+                if payment < 0:
+                    print("———")
+                    print("金額は正の数で入力してください。")
+                    self.title()
+                    self.shop()
+                    return
+                elif payment < total:
+                    print("———")
+                    print("金額が不足しています。")
+                    self.title()
+                    self.shop()
+                    return
+                else:
+                    change = payment - total
+                    print("———")
+                    print(f"ご購入ありがとうございます。おつり{change}円です。")
+                    break
+            except ValueError:
+                print("整数で金額を入力してください。")
+
+vm = Human()
+vm.title()
+vi= Admin()
+def get_key_input():
+    while True:
+        if keyboard.is_pressed("esc"):
+            return "esc"
+        if keyboard.is_pressed("enter"):
+            return "enter"
+        if keyboard.is_pressed("q"):
+            return "q"
+key_pressed = get_key_input()
+if key_pressed == "esc":
+    vi.menu()
+elif key_pressed == "enter":
+    vm.shop()
+elif key_pressed == "q":
+    print("qキーが押されました。qキー押下でシミュレータ終了")
