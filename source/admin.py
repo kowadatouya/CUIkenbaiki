3ea98f9a7ebfa662c895426d2b4f6772c66a2ebc
kowada <t.kowada.sys24@morijyobi.ac.jp>
2025-02-13 15:10:53 +0900

    管理側のテストコード作成close #6 close #10 close #13 close #15

--- source/admin.py
+++ source/admin.py
@@ -1,6 +1,8 @@
 import keyboard
+import time
 class Admin:
     def __init__(self):
+        self.current_state = "admin"
         self.special = 1000
         self.special_pieces = 0
         self.special_sum = 0
@@ -13,12 +15,14 @@ class Admin:
         self.rice = 150
         self.rice_pieces = 0
         self.rice_sum = 0
-    def get_key_input(self):
-        while True:
-            if keyboard.is_pressed('enter'):
-                return 'enter'
-            if keyboard.is_pressed('esc'):
-                return 'esc'
+        self.items = {
+            "1": {"name": '特製ラーメン',"price":self.special,"pieces":self.special_pieces,"sum":self.special_sum},
+            "2": {"name": '醤油ラーメン',"price":self.soi,"pieces":self.soi_pieces,"sum":self.soi_sum},
+            "3": {"name": 'しおラーメン',"price":self.salt,"pieces":self.salt_pieces,"sum":self.salt_sum},
+            "4": {"name": 'ごはん',"price":self.rice,"pieces":self.rice_pieces,"sum":self.rice_sum}
+        }
+        self.sum = self.special_sum + self.soi_sum + self.salt_sum + self.rice_sum
+        self.items_in_cart = {}
     def menu(self):
         print('***********************')
         print('       管理画面')
@@ -26,12 +30,10 @@ class Admin:
         print('======= 商品一覧 =======')
         print('商品      単価  販売数  売上金額')
         print('=======================')
-        print(f'1.特製ラーメン {self.special}円  {self.special_pieces}   {self.special_sum}')
-        print(f'2.醤油ラーメン {self.soi}円   {self.soi_pieces}      {self.soi_sum}')
-        print(f'3.しおラーメン {self.salt}円     {self.salt_pieces}     {self.salt_sum}')
-        print(f'4.ごはん {self.rice}円      {self.rice_pieces}        {self.rice_sum} ')
+        for key, item in self.items.items():
+            print(f"{key}.{item["name"]} {item["price"]}円 {item["pieces"]} {item["sum"]}円")
         print('———')
-        print('総売上金額 80,700円')
+        print(f'総売上金額 {self.sum}円')
         print('=== 管理メニュー ====')
         print('1. 売上をリセットする')
         print('2. 商品の価格を変更する')
@@ -39,8 +41,7 @@ class Admin:
         print('3. 管理画面を終了する')
         print('———')
         self.admin_user()
-    def command1(self):
-        print("管理コード入力:1")
+    def command1(self, skip_menu=False):
         print("===")
         self.special_pieces = 0
         self.special_sum = 0
@@ -51,34 +52,56 @@ class Admin:
         self.rice_pieces = 0
         self.rice_sum = 0
         print("売上をリセットしました。")
-        self.menu()
-    def command2(self):
-        print('管理コード入力:2')
+        if skip_menu == False:
+            self.menu()
+    def command2(self, skip_menu=False):
         print('======= 商品一覧 =======')
         print('商品      単価  販売数  売上金額')
         print('=======================')
-        print('1.特製ラーメン 1000円  0   0円')
-        print('2.醤油ラーメン 780円   0     0円')
-        print('3.しおラーメン 880円     0     0円')
-        print('4.ごはん 150円      0     0円')
+        print(f'1.特製ラーメン {self.special}円  {self.special_pieces}   {self.special_sum}円')
+        print(f'2.醤油ラーメン {self.soi}円   {self.soi_pieces}   {self.soi_sum}円')
+        print(f'3.しおラーメン {self.salt}円   {self.salt_pieces}   {self.salt_sum}円')
+        print(f'4.ごはん {self.rice}円      {self.rice_pieces}   {self.rice_sum}円')
         print('———')
-        input('価格を変更する商品の番号を入力してください。>')
-        input('変更金額を入力してください。>')
-        print('【2.醤油ラーメン 800円】に変更します。')
-        print('よろしいですか(Y/N）>')
-        print('変更しました。')
-        self.menu()
+        pieces = input('価格を変更する商品の番号を入力してください。>')
+        change = input('変更金額を入力してください。>')
+        print(f'【{pieces}. {self.items[pieces]["name"]} {change}円に変更します。')
+        select = input('よろしいですか(Y/N）>')
+        id = pieces
+        if select == 'Y' or 'y':
+            self.items[id]["price"] = change
+            print('変更しました。')
+        elif select == 'N' or 'n':
+            print('キャンセルしました')
+        if skip_menu == False:
+            self.menu()
+    
     def command3(self):
-        print('管理コード入力:3 （Enterキー押下でタイトル画面の表示に戻る）')
-        key = self.get_key_input()
+        def key_input(self):
+            while True:
+                if keyboard.is_pressed('enter'):
+                    return 'enter'
+        key = key_input()
+        print('（Enterキー押下でタイトル画面の表示に戻る）')
         if key == 'enter':
+            time.sleep(1)
             from human import Human
-            Human.title()
+            self = self
+            branch1 = self.items["1"]["price"]
+            branch2 = self.items["2"]["price"]
+            branch3 = self.items["3"]["price"]
+            branch4 = self.items["4"]["price"]
+            vi = Human(self)
+            vi.title()
     def admin_user(self):
-        admin = input()
+        admin = int(input('管理コード入力:'))
         if admin == 1:
             self.command1()
         elif admin == 2:
-            self.command2
+            self.command2()
         elif admin == 3:
             self.command3()
+    def update_sales(self, item_name, quantity, price):
+        if item_name in self.items:
+            self.items[item_name]["pieces"] += quantity
+            self.items[item_name]["sum"] += quantity * price
